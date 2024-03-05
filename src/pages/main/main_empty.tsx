b0c74bd5ca52487e527450b4bded1f27290b9024
Valeria Mukhametdinova <vkvaleria2000@gmail.com>
2024-03-06 02:25:34 +0500

    перенесла header в app, потому что он общий

--- src/pages/main/main_empty.tsx
+++ src/pages/main/main_empty.tsx
@@ -1,44 +1,29 @@
 import CitiesTabs from "../../components/cities_tabs";
-import Header from "../../components/header";
 
 type MainEmptyProps = {
     count_places: number;
     
     cities: string[];
     active_city_id: number;
-
-    is_main: boolean;
-    was_login: -1|0|1; //'-1' - не зашел, '0' - заходит, '1' - зашел 
-    email?: string;
-    favorite?: number;
 }
 
 function MainEmpty(mainEmptyProps:MainEmptyProps): JSX.Element {
     return (
-        <div className="page page--gray page--main">
-            <Header 
-                is_main={mainEmptyProps.is_main} 
-                was_login={mainEmptyProps.was_login} 
-                email={mainEmptyProps.email}
-                favorite={mainEmptyProps.favorite}
-            />
-
-            <main className="page__main page__main--index page__main--index-empty">
-                <h1 className="visually-hidden">Cities</h1>
-                <CitiesTabs cities={mainEmptyProps.cities} active_city_id={mainEmptyProps.active_city_id}/>
-                <div className="cities">
-                    <div className="cities__places-container cities__places-container--empty container">
-                        <section className="cities__no-places">
-                        <div className="cities__status-wrapper tabs__content">
-                            <b className="cities__status">No places to stay available</b>
-                            <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
-                        </div>
-                        </section>
-                        <div className="cities__right-section"></div>
+        <main className="page__main page__main--index page__main--index-empty">
+            <h1 className="visually-hidden">Cities</h1>
+            <CitiesTabs cities={mainEmptyProps.cities} active_city_id={mainEmptyProps.active_city_id}/>
+            <div className="cities">
+                <div className="cities__places-container cities__places-container--empty container">
+                    <section className="cities__no-places">
+                    <div className="cities__status-wrapper tabs__content">
+                        <b className="cities__status">No places to stay available</b>
+                        <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
                     </div>
+                    </section>
+                    <div className="cities__right-section"></div>
                 </div>
-            </main>
-        </div>
+            </div>
+        </main>
     );
 }
 
