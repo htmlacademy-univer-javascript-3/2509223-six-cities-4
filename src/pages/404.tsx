1ea985ab2a4b41d96ddbaee884ac5239e1faf96a
Valeria Mukhametdinova <vkvaleria2000@gmail.com>
2024-03-19 13:46:51 +0500

    создала страницу 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function NotFound(): JSX.Element{
+    return(
+        <Fragment>
+            <h1>
+                404.
+                <br/>
+                <small>Page not found.</small>
+            </h1>
+            
+        </Fragment>
+    );
+}
+
+export default NotFound
